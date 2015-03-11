53dd610c9fc635aa9427a3fe3ca240f23f1b5076
Kizoku-Dev <xkizokux@gmail.com>
2015-03-11 13:32:46 +0100

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -43,7 +43,7 @@ function get_repos(){
 		});
 		
 		//$('#gravatar_img').attr( 'src', 'https://secure.gravatar.com/avatar/'+repos[0].owner.gravatar_id+'?s=512' ).show();
-		$('#gravatar_img').attr( 'src', 'profil.png' ).show();
+		$('#gravatar_img').attr( 'src', 'https://secure.gravatar.com/avatar/43f3fe9a1b21114e77c68825c0f9d475?s=512' ).show();
 		
 		place_divs(  );
 
